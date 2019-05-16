e17d184f00631bd3295a695eeadbcf10cd1968b2
Mac User <MacUser@Users-iMac-282.fios-router.home>
2019-05-15 21:36:26 -0400

    update

--- www/js/global.js
+++ www/js/global.js
@@ -4,39 +4,44 @@ $('#checkSubmit').on('click', function()
     console.log('name is ' + name);
 });
 
-$.ajax({
-    url: 'process/getUsers.php',
-    type: 'POST',
-    data: '',
-    dataType: 'html',
-    success: function(data, textStatus, jqXHR)
-    {
-        console.log(data);
-        /*var jsonObject = JSON.parse(data);
+function displayRecords()
+{
+    $.ajax({
+        url: 'process/getUsers.php',
+        type: 'POST',
+        data: '',
+        dataType: 'html',
+        success: function(data, textStatus, jqXHR)
+        {
+            var jsonObject = JSON.parse(data);
 
-        var table = $('#example1').DataTable({
-            "data": jsonObject,
-            "columns": [
-                {"data": "id"},
-                {"data": "name"}
-            ],
-            "iDisplayLength": 25,
-            "order": [[ 1, "desc" ]],
-            "paging": true,
-            "scrollY": 550,
-            "scrollX": true,
-            "bDestroy": true,
-            "stateSave": true,
-            "autoWidth": true,
-            "deferRender": true
-        });*/
-    },
-    error: function(jqHHR, textStatus, errorThrown)
-    {
-        $('#loadingDiv').hide();
-        $('#errorModal').modal('show');
-        $('.message').text('There was an error conducting your search. Please try again.');
-        console.log('fail: '+ errorThrown);
-        return false;       
-    }
-});
+            var table = $('#example1').DataTable({
+                "data": jsonObject,
+                "columns": [
+                    {"data": ""},
+                    {"data": "id"},
+                    {"data": "name"}
+                ]/*,
+                "iDisplayLength": 25,
+                "order": [[ 1, "desc" ]],
+                "paging": true,
+                "scrollY": 550,
+                "scrollX": true,
+                "bDestroy": true,
+                "stateSave": true,
+                "autoWidth": true,
+                "deferRender": true*/
+            });
+        },
+        error: function(jqHHR, textStatus, errorThrown)
+        {
+            $('#loadingDiv').hide();
+            $('#errorModal').modal('show');
+            $('.message').text('There was an error conducting your search. Please try again.');
+            console.log('fail: '+ errorThrown);
+            return false;       
+        }
+    });
+}
+
+displayRecords();
--- www/process/getUsers.php
+++ www/process/getUsers.php
@@ -1,8 +1,20 @@
 <?php
     include("../include/database.php");
 
-    $query = 'SELECT `id`, `name` From Person';
-    $result = mysqli_query($conn, $query);
+    $query = "SELECT '', id, `name` From Person";
+
+    if($result = $conn->query($query))
+    {
+        $out = array();
+        while($row = $result->fetch_assoc())
+        {
+            $out[] = $row;
+        }
+        echo json_encode($out);
+        $result->free();
+    }
+
+    /*$result = mysqli_query($conn, $query);
 
     $out = array(); 
     while($row = $query->fetch_assoc())
@@ -11,6 +23,6 @@
     }
     echo json_encode($out);
 
-    # close connection
-    $dbc->close();    
+    # close connection*/
+    $conn->close();    
 ?>
